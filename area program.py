02fdd7a9415b8ad9e7e03efce17110c1cbc86dee
Tamizththendral <tamizththendral@gmail.com>
2024-11-28 14:17:31 +0530

    Add files via upload

---
+++ area program.py
@@ -0,0 +1,14 @@
+length=int(input('enter length'))
+breadth=int(input('enter breadth'))
+area_of_rectangle=length*breadth
+print(area_of_rectangle)
+square=int(input('enter the length'))
+area_of_square=square**2
+print(area_of_square)
+radius=int(input('enter the radius'))
+area_of_circle=3.14*radius**2
+print(area_of_circle)
+height=int(input('enter the height'))
+base=int(input('enter the base'))
+area_of_triangle=1/2*base*height
+print(area_of_triangle)
